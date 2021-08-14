7fca4ee793d10894db231d8dae45cc5138ac6548
cristiano-sancho <cristiano-sancho@hotmail.com>
2021-08-14 17:36:32 -0300

    Criação do bucket na AWS com TF

---
+++ Iac/main.tf
@@ -0,0 +1,33 @@
+# HCL - Hashicorp Configuration Language
+# Liguagem declarativa
+
+resource "aws_s3_bucket" "datalake" {
+  # Parametros de configuracao do recurso escolhido
+  bucket = "igti-edc-mod1-datalake"
+  acl = "private"
+
+  server_side_encryption_configuration {    
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    "curso" = "igti"
+  }
+}
+
+
+resource "aws_s3_bucket_object" "code_spark" {
+    bucket = aws_s3_bucket.datalake.id
+    key = "emr-code/pyspark/job_emr.py"
+    acl = "private"
+    source = "../job_emr.py"
+    etag = filemd5("../job_emr.py")    
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ Iac/variables.tf
@@ -0,0 +1,15 @@
+variable "base_bucket_name" {
+  default = "igti-edc-datalake-sancho2"
+}
+
+variable "ambiente" {
+  default = "producao"
+}
+
+variable "numero_conta" {
+  default = "5785585"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
