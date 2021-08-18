4b4a90746f6f3c7266b597190773eab6272fb88c
cristiano-sancho <cristiano-sancho@hotmail.com>
2021-08-18 15:21:47 -0300

    Incluir Glue Crawler 2

--- infrastructure/glue.tf
+++ infrastructure/glue.tf
@@ -7,7 +7,7 @@ resource "aws_glue_job" "job-igti" {
   }
 }
 
-resource "aws_glue_crawler" "igti-edc-mod1-censo" {
+resource "aws_glue_crawler" "igti-edc-mod1-censo-raw" {
   database_name = var.glue_database
   name          = "igti-edc-mod1-censo-raw-crawler"
   role          = aws_iam_role.glue_role.arn
@@ -16,6 +16,17 @@ resource "aws_glue_crawler" "igti-edc-mod1-censo" {
     path = "s3://${aws_s3_bucket.datalake.id}/raw-data/censo"
   }
 }
+
+resource "aws_glue_crawler" "igti-edc-mod1-censo-pqt" {
+  database_name = var.glue_database
+  name          = "igti-edc-mod1-censo-parquet-crawler"
+  role          = aws_iam_role.glue_role.arn
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.datalake.id}/staging-zone/censo"
+  }
+}
+
 
 resource "aws_lakeformation_data_lake_settings" "permission" {
   admins = [aws_iam_role.glue_role.arn]
